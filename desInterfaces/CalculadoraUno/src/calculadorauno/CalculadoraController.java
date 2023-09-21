62d9911fb2b10e35c4e25a4c7415fcafe70ff0d8
Alberto Naranjo <140577660+AlbertoCodex@users.noreply.github.com>
2023-09-22 01:21:08 +0200

    Ejercicio1Calculadora1

---
+++ desInterfaces/CalculadoraUno/src/calculadorauno/CalculadoraController.java
@@ -0,0 +1,108 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package calculadorauno;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author Alberto
+ */
+public class CalculadoraController implements Initializable {
+
+    @FXML
+    private Label labPantalla;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void clic_siete(ActionEvent event) {
+    }
+
+    @FXML
+    private void clic_ocho(ActionEvent event) {
+    }
+
+    @FXML
+    private void clic_nueve(ActionEvent event) {
+    }
+
+    @FXML
+    private void clic_neutro(ActionEvent event) {
+    }
+
+    @FXML
+    private void clic_flecha(ActionEvent event) {
+    }
+
+    @FXML
+    private void clic_cuatro(ActionEvent event) {
+    }
+
+    @FXML
+    private void clic_cinco(ActionEvent event) {
+    }
+
+    @FXML
+    private void clic_seis(ActionEvent event) {
+    }
+
+    @FXML
+    private void clic_multiplicar(ActionEvent event) {
+    }
+
+    @FXML
+    private void clic_dividir(ActionEvent event) {
+    }
+
+    @FXML
+    private void clic_uno(ActionEvent event) {
+    }
+
+    @FXML
+    private void clic_dos(ActionEvent event) {
+    }
+
+    @FXML
+    private void clic_tres(ActionEvent event) {
+    }
+
+    @FXML
+    private void clic_restar(ActionEvent event) {
+    }
+
+    @FXML
+    private void clic_c(ActionEvent event) {
+    }
+
+    @FXML
+    private void clic_cero(ActionEvent event) {
+    }
+
+    @FXML
+    private void clic_punto(ActionEvent event) {
+    }
+
+    @FXML
+    private void clic_sumar(ActionEvent event) {
+    }
+
+    @FXML
+    private void clic_igual(ActionEvent event) {
+    }
+    
+}
