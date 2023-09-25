5cd81c9c870bee31870a0fb32b4e5fb6d1fe87c3
Alberto Naranjo <zenycs3@gmail.com>
2023-09-25 20:51:49 +0200

    ProcesoRemoto project

---
+++ serviciosProcesos/primerTrimestre/procesoRemoto/src/main/java/servicios/procesoremoto/ProcesoSecundario.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package servicios.procesoremoto;
+
+/**
+ *
+ * @author Alberto
+ */
+public class ProcesoSecundario {
+    public static void main (String[] args){
+        System.out.println("Proceso secundario");
+        System.exit(0);
+    }
+}
