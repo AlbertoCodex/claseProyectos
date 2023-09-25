71753a3c031fb6c2a37d45f2ad84267474da81d8
Alberto Naranjo <zenycs3@gmail.com>
2023-09-25 19:24:05 +0200

    ChangeName

--- serviciosProcesos/gestionHotel/src/main/java/servicios/gestionhotel/main.java
+++ serviciosProcesos/gestionHotel/src/main/java/servicios/gestionhotel/main.java
@@ -8,7 +8,7 @@ package servicios.gestionhotel;
  *
  * @author Alberto
  */
-public class GestionHotel {
+public class main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
