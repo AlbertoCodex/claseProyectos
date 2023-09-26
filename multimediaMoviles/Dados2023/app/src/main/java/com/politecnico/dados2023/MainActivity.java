5462ce93f9178506e2581ce57886e6a765106981
Alberto Naranjo <zenycs3@gmail.com>
2023-09-26 19:40:52 +0200

    JavaConditions

--- multimediaMoviles/Dados2023/app/src/main/java/com/politecnico/dados2023/MainActivity.java
+++ multimediaMoviles/Dados2023/app/src/main/java/com/politecnico/dados2023/MainActivity.java
@@ -15,13 +15,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void playGame(View v) {
-        int iD1J1 = 0;
-        int iD2J1 = 4;
+        int iJ1D1 = (int) (Math.random()*6+1);
+        int iJ1D2 = (int) (Math.random()*6+1);
+        int iSeis1 = 0;
+        int iJ2D1 = (int) (Math.random()*6+1);
+        int iJ2D2 = (int) (Math.random()*6+1);
+        int iSeis2 = 0;
+        TextView tvResultado = (TextView) this.findViewById(R.id.tvResultado);
 
-        int iD1J2 = 6;
-        int iD2J2 = 2;
+        if (iJ1D1 == 6) {iSeis1++;}
+        if (iJ1D2 == 6) {iSeis1++;}
+        if (iJ2D1 == 6) {iSeis2++;}
+        if (iJ2D2 == 6) {iSeis2++;}
+        if (iSeis1 > iSeis2) { tvResultado.setText("Jugador 1");
+        } else if (iSeis1 < iSeis2) {tvResultado.setText("Jugador 2");}
+        else if (iSeis1 == iSeis2) {tvResultado.setText("Empate");}
+        else {
+            // Falta cuando la suma son iguales
+            if (iJ1D1 + iJ1D2 > iJ2D1 + iJ2D2) {tvResultado.setText("Jugador 1");}
+            else {tvResultado.setText("Jugador 2");}
+        }
 
-        TextView tvResultado = (TextView) this.findViewById(R.id.textView7);
-        tvResultado.setText("Ha ganado el jugador: " + String.valueOf(1));
     }
 }
