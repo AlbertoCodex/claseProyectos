6a855adab0afc935f6b85c7c944adc4dd869d030
Alberto Naranjo <zenycs3@gmail.com>
2023-09-28 21:45:23 +0200

    Dados2Jugadores

--- multimediaMoviles/Dados2023/app/src/main/java/com/politecnico/dados2023/MainActivity.java
+++ multimediaMoviles/Dados2023/app/src/main/java/com/politecnico/dados2023/MainActivity.java
@@ -22,19 +22,35 @@ public class MainActivity extends AppCompatActivity {
         int iJ2D2 = (int) (Math.random()*6+1);
         int iSeis2 = 0;
         TextView tvResultado = (TextView) this.findViewById(R.id.tvResultado);
+        TextView tvJ1D1 = (TextView) this.findViewById(R.id.tvJ1D1);
+        TextView tvJ1D2 = (TextView) this.findViewById(R.id.tvJ1D2);
+        TextView tvJ2D1 = (TextView) this.findViewById(R.id.tvJ2D1);
+        TextView tvJ2D2 = (TextView) this.findViewById(R.id.tvJ2D2);
+
+        // Pasar int to string
+        tvJ1D1.setText(String.valueOf(iJ1D1));
+        tvJ1D2.setText(String.valueOf(iJ1D2));
+        tvJ2D1.setText(String.valueOf(iJ2D1));
+        tvJ2D2.setText(String.valueOf(iJ2D2));
 
         if (iJ1D1 == 6) {iSeis1++;}
         if (iJ1D2 == 6) {iSeis1++;}
         if (iJ2D1 == 6) {iSeis2++;}
         if (iJ2D2 == 6) {iSeis2++;}
-        if (iSeis1 > iSeis2) { tvResultado.setText("Jugador 1");
-        } else if (iSeis1 < iSeis2) {tvResultado.setText("Jugador 2");}
-        else if (iSeis1 == iSeis2) {tvResultado.setText("Empate");}
-        else {
-            // Falta cuando la suma son iguales
+        if (iSeis1 > iSeis2) { tvResultado.setText("Jugador 1");}
+        else if (iSeis1 == iSeis2 && iSeis1 == 2) {tvResultado.setText("Empate");}
+        else {tvResultado.setText("Jugador 2");}
+
+        if(iSeis1 == 1 && iSeis2 == 1) {
             if (iJ1D1 + iJ1D2 > iJ2D1 + iJ2D2) {tvResultado.setText("Jugador 1");}
+            else if (iJ1D1 + iJ1D2 == iJ2D1 + iJ2D2) {tvResultado.setText("Empate");}
             else {tvResultado.setText("Jugador 2");}
         }
 
+        if(iSeis1 == 0 && iSeis2 == 0) {
+            if (iJ1D1 + iJ1D2 > iJ2D1 + iJ2D2) {tvResultado.setText("Jugador 1");}
+            else if (iJ1D1 + iJ1D2 == iJ2D1 + iJ2D2) {tvResultado.setText("Empate");}
+            else {tvResultado.setText("Jugador 2");}
+        }
     }
 }
