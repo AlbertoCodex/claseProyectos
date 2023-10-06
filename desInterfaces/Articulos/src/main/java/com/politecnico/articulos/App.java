bf0655d2687888c8f55f6f1c356b111fe9d3be3b
Alberto Naranjo <zenycs3@gmail.com>
2023-10-06 18:53:58 +0200

    AddEjemplos y VistaArticulos

--- desInterfaces/Articulos/src/main/java/com/politecnico/articulos/App.java
+++ desInterfaces/Articulos/src/main/java/com/politecnico/articulos/App.java
@@ -31,11 +31,12 @@ public class App extends Application {
     // Datos ejemplo -- Continue
 
     public App() {
-        datosArticulo.add(new Articulo("", "", "", "", 0));
-        datosArticulo.add(new Articulo("", "", "", "", 0));
-        datosArticulo.add(new Articulo("", "", "", "", 0));
-        datosArticulo.add(new Articulo("", "", "", "", 0));
-        datosArticulo.add(new Articulo("", "", "", "", 0));;
+        datosArticulo.add(new Articulo("Codigo", "Descripcion", "Fabricante", "Categoria", 0));
+        datosArticulo.add(new Articulo("1234", "aaaaa", "BMW", "Coche", 300));
+        datosArticulo.add(new Articulo("1111", "bbbbb", "VIC", "Boligrafo", 0));
+        datosArticulo.add(new Articulo("2131", "ccccc", "Pascual", "Leche", 0));
+        datosArticulo.add(new Articulo("5435", "ddddd", "Raton", "Animal", 0));;
+        
     }
     
 
@@ -44,6 +45,7 @@ public class App extends Application {
         scene = new Scene(loadFXML("articulos"), 640, 480);
         stage.setScene(scene);
         stage.show();
+        muestraVistaArticulo();
     }
 
     static void setRoot(String fxml) throws IOException {
@@ -57,7 +59,21 @@ public class App extends Application {
 
     public static void main(String[] args) {
         launch();
-        
+    }
+    
+    public void muestraVistaArticulo(){
+        FXMLLoader loader = new FXMLLoader();
+        URL location = App.class.getResource("com.poliecnico.articulos/articulos.fxml");
+        loader.setLocation(location);
+        try {
+            vistaArticulo = loader.load();
+        } catch (IOException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public ObservableList getDatosArticulo() {
+        return datosArticulo;
     }
 
 }
