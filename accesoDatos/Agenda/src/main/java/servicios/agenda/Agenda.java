fd147214b0ab475545c6e563b9343854d65fcb3e
Alberto Naranjo <zenycs3@gmail.com>
2023-10-13 23:50:50 +0200

    Casi completo, el bw falla

--- accesoDatos/Agenda/src/main/java/servicios/agenda/Agenda.java
+++ accesoDatos/Agenda/src/main/java/servicios/agenda/Agenda.java
@@ -4,55 +4,54 @@
 
 package servicios.agenda;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author Alberto
  */
 public class Agenda {
-    String nombre;
-    int numero;
+    HashMap<String, String> agendaContactos = new HashMap<String, String>();
 
     public Agenda() {
     }
 
-    public void insertar(String nombre, int numero) {
+    public String insertar(String nombre, String numero) {
+        Map.Entry<String,String> lastEntry = null;
+        agendaContactos.put(nombre, numero);
+        lastEntry = agendaContactos.entrySet().stream().reduce((first,second) -> second).get();
+        return lastEntry.toString();
     }
     
     public void buscar(String nombre) {
+        System.out.println(nombre + " " + agendaContactos.get(nombre));
     }
     
     public void longitud() {
+        System.out.println("Hay un total de " + agendaContactos.size() + " contactos");
     }
     
     public void ver() {
+        for (String i : agendaContactos.keySet()) {
+            System.out.println("key: " + i + " value: " + agendaContactos.get(i));
+        }
+    }
+    
+    public void borrar(String nombre) {
+        agendaContactos.remove(nombre);
+        System.out.println("Se ha eliminado de la agenda a: " + nombre);
         
     }
     
-    public void borrar() {
-        
-    }
-    
-    public void modificar(String nombre, int numero) {
-        
+    public void modificar(String nombre, String numero) {
+        System.out.println("El contacto " + nombre + " con numero de telefono: " 
+                + agendaContactos.get(nombre) + " ha sido actualizado" );
+        agendaContactos.put(nombre, numero);
+        System.out.println("El nuevo telefono de " + nombre + " es: " + agendaContactos.get(nombre));
     }
     
     public void exportar() {
         
-    }
-    
-    public String getNombre() {
-        return nombre;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setNumero(int numero) {
-        this.numero = numero;
     }
 }
--- accesoDatos/Agenda/src/main/java/servicios/agenda/main.java
+++ accesoDatos/Agenda/src/main/java/servicios/agenda/main.java
@@ -11,6 +11,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.Scanner;
 
 /**
@@ -18,17 +19,17 @@ import java.util.Scanner;
  * @author Alberto
  */
 public class main {
-    
+
     public static void main(String[] args) throws IOException {
         Agenda agenda = new Agenda();
-        menu(args[0]);
+        menu(args[0], agenda);
     }
-    
+
     public static String leer(File f) throws FileNotFoundException, IOException {
         BufferedReader br = new BufferedReader(new FileReader(f));
         String line = "";
         String texto = "";
-        if(f.exists() && f.isFile()) {
+        if (f.exists() && f.isFile()) {
             while ((line = br.readLine()) != null) {
                 texto += line + '\n';
             }
@@ -36,45 +37,136 @@ public class main {
         br.close();
         return texto;
     }
-    
+
     public static void crearCopia(File fichero) throws IOException {
-        String copia = fichero.getName().replaceFirst("[.][^.]+$", "")+"_copia.txt";
+        String copia = fichero.getName().replaceFirst("[.][^.]+$", "") + "_copia.txt";
         File copiaFile = new File(copia);
         FileWriter fw = new FileWriter(copia, false);
         BufferedWriter bw = new BufferedWriter(fw);
-        
+
         System.out.println("Se ha realizado una copia en " + copia);
         bw.write(leer(fichero));
         bw.close();
     }
-    
-    
-    
-    public static void menu(String agendaFichero) throws IOException {
+
+    public static void menu(String agendaFichero, Agenda agenda) throws IOException {
         Scanner sc = new Scanner(System.in);
         File f = new File(agendaFichero);
         int x = 1;
-        
-        while(x != 0) {
+
+        while (x != 0) {
             System.out.println(leer(f));;
             System.out.println("0 para salir" + '\n'
                     + "1 para hacer cambios en el fichero" + '\n'
                     + "2 para crear una copia" + '\n');
-            x = Integer.parseInt(sc.nextLine());
-            
-            switch(x) {
+            x = sc.nextInt();
+
+            switch (x) {
                 case 1:
-                    
+                    menuCambios(agenda, f);
                     break;
-                    
+
                 case 2:
                     crearCopia(f);
                     break;
-                    
+
                 default:
                     break;
             }
         }
     }
-    
+
+    public static void menuCambios(Agenda agenda, File f) throws IOException {
+        cargarHash(agenda, f);
+        String nombreEjemplo = "Test Borrar";
+        String numeroModificado = "543254534534";
+        Scanner sc = new Scanner(System.in);
+        FileWriter fw = new FileWriter(f, true);
+        BufferedWriter bw = new BufferedWriter(fw);
+        int x = -1;
+
+        while (x != 0) {
+            System.out.println("0 para salir" + '\n'
+                    + "1 para insertar un contacto" + '\n'
+                    + "2 para buscar un contacto" + '\n'
+                    + "3 para ver cuantos contactos hay" + '\n'
+                    + "4 para ver la agenda" + '\n'
+                    + "5 para borrar un contacto de la agenda" + '\n'
+                    + "6 para modificar un contacto de la agenda");
+            x = sc.nextInt();
+
+            switch (x) {
+                case 1:
+                    bw = new BufferedWriter(fw);
+                    bw.write(agenda.insertar(nombreEjemplo, String.valueOf((int) (Math.random() * 999999999))));
+                    bw.close();
+                    cargarHash(agenda, f);
+                    break;
+                case 2:
+                    agenda.buscar("Alberto Naranjo,");
+                    break;
+                case 3:
+                    agenda.longitud();
+                    break;
+                case 4:
+                    agenda.ver();
+                    break;
+                case 5:
+                    agenda.borrar(nombreEjemplo);
+                    deleteContact(f, nombreEjemplo);
+                    break;
+                case 6:
+                    bw = new BufferedWriter(fw);
+                    deleteContact(f, nombreEjemplo);
+                    bw.newLine();
+                    bw.write(agenda.insertar(nombreEjemplo, numeroModificado));
+                    bw.close();
+                    cargarHash(agenda, f);
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+    }
+
+    public static void cargarHash(Agenda agenda, File f) throws IOException {
+        String texto = leer(f);
+        System.out.println(texto);
+        String nombre = "";
+        String numero = "";
+        String value = "";
+
+        for (int i = 0; i < texto.length(); i++) {
+            value += texto.charAt(i);
+            if (texto.charAt(i) == ',' || texto.charAt(i) == '=') {
+                nombre = value;
+                value = "";
+            }
+            if (texto.charAt(i) == '\n') {
+                numero = value;
+                agenda.insertar(nombre, numero);
+                nombre = "";
+                numero = "";
+                value = "";
+            }
+        }
+    }
+
+    public static void deleteContact(File f, String nombre) throws IOException {
+        RandomAccessFile file = new RandomAccessFile(f, "rw");
+        String linea;
+        String texto = "";
+        while ((linea = file.readLine()) != null) {
+            if (linea.startsWith(nombre)) {
+                continue;
+            }
+            texto += linea + "\n";
+        }
+        System.out.println(texto);
+        BufferedWriter bw = new BufferedWriter(new FileWriter(f));
+        bw.write(texto);
+        file.close();
+        bw.close();
+    }
 }
