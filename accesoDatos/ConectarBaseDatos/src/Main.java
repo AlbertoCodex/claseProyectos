a74e48a9e2a92ca85cdbecfbc2bab2725913be70
Alberto Naranjo <zenycs3@gmail.com>
2023-10-30 13:32:27 +0100

    JDBC connection

--- accesoDatos/ConectarBaseDatos/src/Main.java
+++ accesoDatos/ConectarBaseDatos/src/Main.java
@@ -3,8 +3,10 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         String urlConnection = "jdbc:mysql://localhost:3306/?user=root";
-        
+        try (Connection c = DriverManager.getConnection(urlConnection)) {
+            System.out.println("Conexion realizada");
+        }
     }
 }
