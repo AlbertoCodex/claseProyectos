50b503165b5b48b0e7ccf2b39ab875e7e091bcf5
Alberto Naranjo <zenycs3@gmail.com>
2023-10-30 13:48:31 +0100

    Add library conector + crear conexion

--- accesoDatos/ConectarBaseDatos/src/Main.java
+++ accesoDatos/ConectarBaseDatos/src/Main.java
@@ -1,11 +1,17 @@
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        String urlConnection = "jdbc:mysql://localhost:3306/?user=root";
-        try (Connection c = DriverManager.getConnection(urlConnection)) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Pon el usuario");
+        String user = sc.nextLine();
+        System.out.println("Pon la contraseña");
+        String pwd = sc.nextLine();
+        String urlConnection = "jdbc:mysql://localhost:3306";
+        try (Connection c = DriverManager.getConnection(urlConnection, user, pwd)) {
             System.out.println("Conexion realizada");
         }
     }
