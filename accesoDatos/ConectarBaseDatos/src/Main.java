9722a1c785cec6aa3f5c85b9bcf966880b927f9e
Alberto Naranjo <zenycs3@gmail.com>
2023-10-30 13:20:53 +0100

    Crear proyecto

---
+++ accesoDatos/ConectarBaseDatos/src/Main.java
@@ -0,0 +1,10 @@
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+        String urlConnection = "jdbc:mysql://localhost:3306/?user=root";
+        
+    }
+}
